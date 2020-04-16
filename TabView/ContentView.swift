7c3e4b1c29c980e863feb36d308d609893ddc4ea
Dhanraj Chavan <dc6707914@gmail.com>
2020-04-16 13:22:53 +0530

    Initial Commit

---
+++ TabView/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TabView
+//
+//  Created by Dhanraj Sudhir Chavan on 16/04/20.
+//  Copyright © 2020 Dhanraj Chavan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
